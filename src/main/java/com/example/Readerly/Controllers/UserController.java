86cddf811e94b78e7dc5ed3de1feab62729afb00
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-11-20 15:12:58 +0530

    User login

--- src/main/java/com/example/Readerly/Controllers/UserController.java
+++ src/main/java/com/example/Readerly/Controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.Readerly.Controllers;
 
+import com.example.Readerly.DTO.LoginRequest;
 import com.example.Readerly.Entities.User;
+import com.example.Readerly.Exceptions.InvalidCredentialException;
 import com.example.Readerly.Services.UserService;
 import com.mongodb.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +27,26 @@ public class UserController {
         try {
             String message = userService.signUp(user);
             return ResponseEntity.status(HttpStatus.CREATED).body(message);
-        } catch (DuplicateKeyException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        } catch (Exception e) {
+            if(e instanceof DuplicateKeyException) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"An error occurred.\"}");
         }
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            String token = userService.login(loginRequest.email, loginRequest.password);
+            return ResponseEntity.ok().body("{\"token\": \"" + token + "\"}");
+        } catch (Exception e) {
+            if(e instanceof InvalidCredentialException) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
 
