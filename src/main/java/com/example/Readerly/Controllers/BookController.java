4075ff19cdd04d6eb19482d9a169358b1fa36ebc
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-11-20 13:14:20 +0530

    connected with mongo , first DB ops

---
+++ src/main/java/com/example/Readerly/Controllers/BookController.java
@@ -0,0 +1,33 @@
+package com.example.Readerly.Controllers;
+
+import com.example.Readerly.Entities.Book;
+import com.example.Readerly.Services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book savedBook = bookService.addBook(book);
+        return ResponseEntity.ok(savedBook);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok(books);
+    }
+}
